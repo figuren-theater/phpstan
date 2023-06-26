2772121bb48d9ebad9defc32b24de039c48bb407
Carsten Bach <mail@carsten-bach.de>
2023-06-26 15:50:12 +0200

    Develop (#1)
    * Include custom stubs for the figuren.theater platform
    
    * Add example

---
+++ ft-stubs.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Constants used on the figuren-theater/ft-platform
+ */
+
+//
+// figuren-theater/ft-platform !
+//
+
+// ./wp-config.php
+define( 'FT_ROOT_DIR', __DIR__ );
+define( 'FT_WP_DIR', '/wp' );
+
+// ./wp-config.default.php
+define( 'FT_VENDOR_DIR', FT_ROOT_DIR . '/vendor' );
+define( 'FT_VENDOR_URL', WP_CONTENT_URL . '/v' ); // Symlinked to DOCROOT/vendor.
+
+// ./wp-config.{{ENV}}.php
+define( 'FT_MAINTENANCE_MODE', false );
+
+// ./content/mu-plugins/00-ft-platform__constants.php
+define( 'FT_PLATTFORM_VERSION', '3.0.2' );
+
+//
+// figuren-theater/ft-maitenance !
+//
+
+// ./content/mu-plugins/FT/ft-maintenance/inc/mode/namespace.php
+// ./content/mu-plugins/FT/ft-maintenance/inc/mode/error-template.php
+// ./content/db-error.php
+// ./content/maintenance.php
+// ./content/php-error.php
+define( 'FT_ERROR_MAIL_TO', 'f.t web-Crew <ft@localhost.test>' );
+define( 'FT_ERROR_MAIL_FROM', 'ft@localhost.test' );
+define( 'FT_ERROR_MAIL_INTERVAL', 300 );
+define( 'FT_ERROR_SUPPRESS_EMAIL', true );
